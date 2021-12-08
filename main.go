fd4616d0c414d73d30a1c65a266e6bc3f653be38
kindf <1304298803@qq.com>
2021-12-08 13:55:18 +0800

    #feature 新增关于页面

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func handlerfunc(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "<h1>Hello, here is goblog</h1>")
+    if r.URL.Path == "/" {
+        fmt.Fprint(w, "<h1>Hello, here is goblog</h1>")
+    } else if r.URL.Path == "/about" {
+        fmt.Fprint(w, "<h1>about 页面</h1>")
+    } else {
+        fmt.Fprint(w, "<h1>请求页面不存在</h1>")
+    }
 }
 
 func main() {
