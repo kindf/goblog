a8a222e7df1800accb20c16074288486b03cb9aa
kindf <1304298803@qq.com>
2021-12-08 15:22:09 +0800

    #feature 新建博文表单

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
     "github.com/gorilla/mux"
 )
 
+ var router = mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Hello, welcome to goblog</h1>")
 }
@@ -34,6 +36,27 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "create article")
 }
 
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "create blog")
+    html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+</head>
+<body>
+    <form action="%s" method="post">
+        <p><input type="text" name="title"></p>
+        <p><textarea name="body" cols="30" rows="10"></textarea></p>
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+    storeURL, _ := router.Get("articles.store").URL()
+    fmt.Fprintf(w, html, storeURL)
+}
+
 func forceHTMLMiddleware(h http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -51,7 +74,6 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 
 func main() {
-    router := mux.NewRouter()
 
     router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
     router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
@@ -59,6 +81,7 @@ func main() {
     router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
     router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
     router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+    router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
     router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
     router.Use(forceHTMLMiddleware)
