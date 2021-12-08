37306271f02768595c7f6d5d16cfb651910999bd
kindf <1304298803@qq.com>
2021-12-08 14:36:46 +0800

    #feature 集成Gorilla Mux路由

--- main.go
+++ main.go
@@ -3,21 +3,50 @@ package main
 import (
     "net/http"
     "fmt"
+    "github.com/gorilla/mux"
 )
 
-func handlerfunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    if r.URL.Path == "/" {
-        fmt.Fprint(w, "<h1>Hello, here is goblog</h1>")
-    } else if r.URL.Path == "/about" {
-        fmt.Fprint(w, "<h1>about 页面</h1>")
-    } else {
-        w.WriteHeader(http.StatusNotFound)
-        fmt.Fprint(w, "<h1>请求页面不存在</h1>")
-    }
+    fmt.Fprintf(w, "<h1>Hello, welcome to goblog</h1>")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    fmt.Fprintf(w, "<h1>Here is about page</h1>")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    w.WriteHeader(http.StatusNotFound)
+    fmt.Fprintf(w, "<h1>Page not found</h1>")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id := vars["id"]
+    fmt.Fprintf(w, "article ID: "+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "article list")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "create article")
 }
 
 func main() {
-    http.HandleFunc("/", handlerfunc)
-    http.ListenAndServe(":8080", nil)
+    router := mux.NewRouter()
+
+    router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+    router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+
+    router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+    router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+    router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+
+    router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+    http.ListenAndServe(":8080", router)
 }
