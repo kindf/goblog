8f778c8a4ff95b22fbd134d3d8b8540e766cfdb0
kindf <1304298803@qq.com>
2021-12-13 21:58:21 +0800

    #feature 美化文章列表

--- pkg/routes/web.go
+++ pkg/routes/web.go
@@ -4,10 +4,13 @@ import (
     "net/http"
     "github.com/gorilla/mux"
     "goblog/app/http/controllers"
-    "goblog/app/http/middlewares"
+    //"goblog/app/http/middlewares"
 )
 
 func RegisterWebRoutes(r *mux.Router){
+
+    //r.Use(middlewares.ForceHTML)
+
     pc := new(controllers.PagesController)
     r.HandleFunc("/", pc.Home).Methods("GET").Name("home")
     r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
@@ -15,15 +18,13 @@ func RegisterWebRoutes(r *mux.Router){
     
     ac := new(controllers.ArticlesController)
     r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
-
     r.HandleFunc("/articles", ac.Index).Methods("GET").Name("articles.index")
-
     r.HandleFunc("/articles/create", ac.Create).Methods("GET").Name("articles.create")
     r.HandleFunc("/articles", ac.Store).Methods("POST").Name("articles.store")
-
     r.HandleFunc("/articles/{id:[0-9]+}/edit", ac.Edit).Methods("GET").Name("articles.edit")
     r.HandleFunc("/articles/{id:[0-9]+}", ac.Update).Methods("POST").Name("articles.update")
-
     r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
-    r.Use(middlewares.ForceHTML)
+
+    r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
+    r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
 }
